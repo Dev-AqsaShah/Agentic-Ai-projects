967dc387d29200cb44bc4ad577f98de42470a808
aqsashah <aqsanor665@gmail.com>
2025-06-10 21:54:28 +0500

    .

--- object-detection/app/page.tsx
+++ object-detection/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
-import '@/styles/globals.css'
+import './globals.css';
 
 
 export default function Home() {
   return (
-   <main className=" ">
-
+   <main className="flex  min-h-screen flex-col items-center p-8">
+      <h1
+      className="font-extrabold text-3x1 md:text-6x1 lg:text-8x1 tracking-tighter md:px-6 text-center"
+      >Thief Detection Alarm</h1>
    </main>
   );
 }
